24db6d81e6fd0dee82921d83846e49c3135227b4
Esteban <esgaanza98@gmail.com>
2021-04-16 23:33:40 +1000

    test and dev

---
+++ ContributionRateGeneration/build/CMakeFiles/scalesextract.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scalesextract.dir/scalesextract.cpp.o"
+  "CMakeFiles/scalesextract.dir/rply.c.o"
+  "scalesextract.pdb"
+  "scalesextract"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/scalesextract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
